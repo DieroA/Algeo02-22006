2dda96791d8e4df1ccc478506d66b700292a4c3a
Agil Fadillah Sabri <agilfadillahsabri03@gmail.com>
2023-11-16 14:04:54 +0700

    feat: Bisa menggunakan dataset yang sama secara berulang

--- src/server.py
+++ src/server.py
@@ -26,70 +26,64 @@ def index():
         dataset = request.files.getlist("folder_dataset")
         query = request.files["query_img"]
         
+        dir_name = 'src/static/dataset/'
+
         no_data_input = not dataset[0]
         no_query_input = not query
-        if no_data_input or no_query_input:
-            return render_template("index.html", error_dataset_empty=no_data_input, error_query_empty=no_query_input)
+        start = time.time()
+        if no_data_input and not os.listdir(dir_name):
+            return render_template("index.html", error_dataset_empty=no_data_input)
+        else:
+            if not no_data_input:
+                # Save dataset images and delete previous dataset and features
+                parent_dir = os.path.basename(os.path.dirname(dataset[0].filename))
+                uploaded_dataset_path = "src/static/dataset/" + parent_dir
+                if (os.path.isdir(uploaded_dataset_path)):
+                    shutil.rmtree(uploaded_dataset_path)
+                textures = [feature for feature in os.listdir("src/static/feature/texture")]
+                for feature in textures:
+                    os.remove(os.path.join("src/static/feature/texture", feature))
+                colors = [feature for feature in os.listdir("src/static/feature/hsv")]
+                for feature in colors:
+                    os.remove(os.path.join("src/static/feature/hsv", feature))
+
+                os.makedirs(uploaded_dataset_path, exist_ok=True)
+                uploaded_dataset_path = "src/static/dataset/"
+                for images in dataset:
+                    img = Image.open(images.stream)
+                    img.save(uploaded_dataset_path + images.filename)
+                
+                # Extract features from dataset
+                for img_path in sorted(Path("src/static/dataset/" + parent_dir).glob("*.jpg")):
+                    print(img_path)
+                    feature = fe.extractTexture(img=Image.open(img_path))
+                    feature_path = Path("src/static/feature/texture") / (img_path.stem + "_texture" + ".npy") 
+                    np.save(feature_path, feature)
+            
+                for img_path in sorted(Path("src/static/dataset/" + parent_dir).glob("*.jpg")):
+                    print(img_path)
+                    feature = fe.extractHSV(img=Image.open(img_path))
+                    feature_path = Path("src/static/feature/hsv") / (img_path.stem + "_hsv" + ".npy") 
+                    np.save(feature_path, feature)
+        if no_query_input:
+            return render_template("index.html", error_query_empty=no_query_input)
+
+        # Save query image
         
-        # Delete previous dataset and features
-        parent_dir = os.path.basename(os.path.dirname(dataset[0].filename))
-        uploaded_dataset_path = "src/static/dataset/" + parent_dir
-        if (os.path.isdir(uploaded_dataset_path)):
-            shutil.rmtree(uploaded_dataset_path)
-        textures = [feature for feature in os.listdir("src/static/feature/texture")]
-        for feature in textures:
-            os.remove(os.path.join("src/static/feature/texture", feature))
-        colors = [feature for feature in os.listdir("src/static/feature/hsv")]
-        for feature in colors:
-            os.remove(os.path.join("src/static/feature/hsv", feature))
-        
-        # Save query and dataset images
         img = Image.open(query.stream)
         uploaded_img_path = "src/static/query/"
         img_name = datetime.now().isoformat().replace(":",".") + "_" + query.filename
         img.save(uploaded_img_path + img_name)
         
-        os.makedirs(uploaded_dataset_path, exist_ok=True)
-        uploaded_dataset_path = "src/static/dataset/"
-        for images in dataset:
-            img = Image.open(images.stream)
-            img.save(uploaded_dataset_path + images.filename)
-        
-        features_hsv = []
-        features_texture = []
+        # Run search on database
         img_paths = []
-        
-        start = time.time()
-        
-        # Extract features from dataset
         if request.form.get("cbir-by-texture"):
-            for img_path in sorted(Path("src/static/dataset/" + parent_dir).glob("*.jpg")):
-                print(img_path)
-                feature = fe.extractTexture(img=Image.open(img_path))
-                feature_path = Path("src/static/feature/texture") / (img_path.stem + "_texture" + ".npy") 
-                np.save(feature_path, feature)
-            
+            features_texture = []
             for feature_path in Path("src/static/feature/texture").glob("*.npy"):
                 features_texture.append(np.load(feature_path))
                 img_paths.append(feature_path.stem.replace("_texture", "")+ ".jpg")
-            
             features_texture = np.array(features_texture)
-        
-        else:
-            for img_path in sorted(Path("src/static/dataset/" + parent_dir).glob("*.jpg")):
-                print(img_path)
-                feature = fe.extractHSV(img=Image.open(img_path))
-                feature_path = Path("src/static/feature/hsv") / (img_path.stem + "_hsv" + ".npy") 
-                np.save(feature_path, feature)
-            
-            for feature_path in Path("src/static/feature/hsv").glob("*.npy"):
-                features_hsv.append(np.load(feature_path))
-                img_paths.append(feature_path.stem.replace("_hsv", "")+ ".jpg")
-            
-            features_hsv = np.array(features_hsv)
-        
-        # Run search on database
-        if request.form.get("cbir-by-texture"):
+
             img = Image.open("src/static/query/" + img_name)
             query = fe.extractTexture(img)
             dists = np.empty([len(features_texture)])
@@ -101,6 +95,12 @@ def index():
                 else:
                     dists[i] = cbt.Cosine_Similarity(query, features_texture[i])
         else:
+            features_hsv = []
+            for feature_path in Path("src/static/feature/hsv").glob("*.npy"):
+                features_hsv.append(np.load(feature_path))
+                img_paths.append(feature_path.stem.replace("_hsv", "")+ ".jpg")
+            features_hsv = np.array(features_hsv)
+
             img = Image.open("src/static/query/" + img_name)
             query = fe.extractHSV(img)
             dists = np.empty([len(features_hsv)])
@@ -121,7 +121,7 @@ def index():
         
         # Paginate the result
         page = int(request.args.get('page', 1))
-        per_page = 5
+        per_page = 20
         items = paginate(page, per_page, scores)
         total_items = len(scores)
         total_pages = (total_items - 1) // per_page + 1
@@ -132,7 +132,7 @@ def index():
                                pagination = {'current_page' : page, 'per_page' : per_page, 'total_pages' : total_pages, 'total_items' : total_items})
     else:
         page = int(request.args.get('page', 1))
-        per_page = 5
+        per_page = 20
         items = paginate(page, per_page, scores)
         total_items = len(scores)
         total_pages = (total_items - 1) // per_page + 1
